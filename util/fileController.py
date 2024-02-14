9a85b475de78b38b254b63c57ea79105d4b15cbf
Shalo-K <ratna.xenon@gmail.com>
2024-02-14 15:35:39 +0900

    add extra config

--- util/fileController.py
+++ util/fileController.py
@@ -19,7 +19,6 @@ class AplConst:
     '''
     def __init__(self, aplPath):
         self.aplConst = input_json(aplPath + "/conf/aplConstants.json")
-        #self.aplConst = AttrDict(input_json(aplPath + "/conf/aplConstants.json"))
     
     def get(self, key):
         '''
@@ -35,6 +34,40 @@ class AplConst:
         value : String 取得した定数の結果
         '''
         value = self.aplConst
+        keys = key.split(".")
+        for name in keys:
+            if (value == None):
+                break
+            value = value.get(name)
+        return value
+
+
+class ExtraConfig:
+    '''
+    アプリケーション定数とは異なるjson形式のConfig値を取得するクラス。
+    jsonファイルはconf配下にあるファイル名(拡張子を含まない)を指定する。
+
+    Attributes
+    ----------
+    exConf : Config値を格納するオブジェクト
+    '''
+    def __init__(self, aplPath, fileName):
+        self.exConf = input_json(aplPath + "/conf/" + fileName + ".json")
+    
+    def get(self, key):
+        '''
+        指定された名前に紐づく定数(String)を返却する。
+        見つからない場合はNoneを返却する。
+
+        Parameters
+        ----------
+        key : String 取得する定数の名称
+
+        Returns
+        -------
+        value : String 取得した定数の結果
+        '''
+        value = self.exConf
         keys = key.split(".")
         for name in keys:
             if (value == None):
